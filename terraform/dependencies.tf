f3d534782bc3b761b29aa8a895d2d065b5d6ef09
ishuar <ishansharma887@gmail.com>
2023-08-02 02:54:33 +0200

    remove allow http from anywhere NSG rule , unwanted

--- terraform/dependencies.tf
+++ terraform/dependencies.tf
@@ -48,24 +48,6 @@ resource "azurerm_network_security_rule" "lb_to_webservers" {
   destination_address_prefixes = azurerm_subnet.webservers.address_prefixes
 }
 
-## ? https://aquasecurity.github.io/trivy/v0.44/docs/configuration/filtering/#by-inline-comments if migrating to trivy
-#tfsec:ignore:azure-network-ssh-blocked-from-internet
-#tfsec:ignore:azure-network-disable-rdp-from-internet
-#tfsec:ignore:azure-network-no-public-ingress
-resource "azurerm_network_security_rule" "http" {
-  access                       = "Allow"
-  direction                    = "Inbound"
-  name                         = "AllowHTTP"
-  priority                     = 102
-  protocol                     = "Tcp"
-  source_port_range            = "*"
-  source_address_prefix        = "*"
-  destination_port_ranges      = [80]
-  resource_group_name          = azurerm_resource_group.main.name
-  network_security_group_name  = azurerm_network_security_group.webserver.name
-  destination_address_prefixes = azurerm_subnet.webservers.address_prefixes ## Allowed HTTP on the subnet level, could be hardened on NIC level.
-}
-
 resource "azurerm_network_interface_security_group_association" "webserver" {
   for_each = toset(local.webservers)
 

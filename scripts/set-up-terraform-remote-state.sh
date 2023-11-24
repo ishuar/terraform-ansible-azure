d9419f67accb991bcf90dac9385299efe6e4b363
Ishan Sharma <38211728+ishuar@users.noreply.github.com>
2023-11-24 20:47:48 +0100

    fix: set up remote backend script
    incorrect operator in resource group creation , az group exists returns back boolean

--- scripts/set-up-terraform-remote-state.sh
+++ scripts/set-up-terraform-remote-state.sh
@@ -2,23 +2,23 @@
 
 ##? This script will help you to create terraform remote azurerm backend resources.
 ##? Please go through the notifications/notes available in the script.
-##? Script will create and check if existing : Resource Group, Storage Account and Storage Container.
-##? To Over-ride the names for the resources, kindly set the respected environment variables.
+##? Script will create and check if existing: Resource Group, Storage Account, and Storage Container.
+##? To Override the names for the resources, kindly set the respective environment variables.
 ##? LOCATION :: RESOURCE_GROUP_NAME :: STORAGE_ACCOUNT_NAME :: CONTAINER_NAME
 
 set -ue
 
-printf '\e[1;31m%-6s\e[0m\n' "### IMPORTANT NOTE ###: Please login to azure first using az login and set up correct azure subscription"
+printf '\e[1;31m%-6s\e[0m\n' "### IMPORTANT NOTE ###: Please login to Azure first using az login and set up the correct Azure subscription"
 printf '\e[1;31m%-6s\e[0m\n' "az login                                         => Login to azure cli."
 printf '\e[1;31m%-6s\e[0m\n' "az account list --output table                   => Check which Azure accounts/subscriptions you have."
 printf '\e[1;31m%-6s\e[0m\n' "az account set -s <your-azure-subscription-id>   => Set the right azure account."
 
 cat <<-EOF
 
-Default Values for locaion, resource group, storage account and container name are set..
-To over ride the values, please export below environment variables with required values:
+Default Values for location, resource group, storage account, and container name are set...
+To override the values, please export below environment variables with the required values:
 
-### YOUR VALUE HAS TO BE REPLACED WITH YOUR REQUIRED INPUT ###
+### "YOUR VALUE" HAS TO BE REPLACED WITH YOUR REQUIRED INPUT ###
 
 export LOCATION="YOUR VALUE"
 export RESOURCE_GROUP_NAME="YOUR VALUE"
@@ -27,7 +27,7 @@ export CONTAINER_NAME="YOUR VALUE"
 
 EOF
 
-printf '\e[1;32m%-6s\e[0m' "Kindly Read above Info and Press yes or y to continue: "
+printf '\e[1;32m%-6s\e[0m' "Kindly Read the above Info and Press yes or y to continue: "
 read -r RESPONSE
 echo ""
 # LOWER_CASE_RESPONSE="$(echo "$RESPONSE" | tr '[:upper:]' '[:lower:]')"
@@ -44,14 +44,14 @@ if [[ "$LOWER_CASE_RESPONSE" == "yes" || "$LOWER_CASE_RESPONSE" == "y" ]]; then
   printf "#### Creating Storage Account for Terraform backend configuration ####\n"
   printf "#######################################################################\n\n"
   if [[ $(command -v az) ]]; then
-    # Check and Create resource group
-    if ! az group exists --name "$RESOURCE_GROUP_NAME" &>/dev/null; then
-      az group create --name "$RESOURCE_GROUP_NAME" --location "$LOCATION"
-    else
+    # Check and Create a resource group
+    if az group exists --name "$RESOURCE_GROUP_NAME"; then
       echo "-> Resource group with name $RESOURCE_GROUP_NAME already exists"
+    else
+      az group create --name "$RESOURCE_GROUP_NAME" --location "$LOCATION"
     fi
 
-    # Create and Check storage account
+    # Create and Check the storage account
     if ! az storage account show --resource-group "$RESOURCE_GROUP_NAME" --name "$STORAGE_ACCOUNT_NAME" &>/dev/null; then
       az storage account create --resource-group "$RESOURCE_GROUP_NAME" --name "$STORAGE_ACCOUNT_NAME" --sku Standard_LRS --encryption-services blob
     else
@@ -78,7 +78,7 @@ if [[ "$LOWER_CASE_RESPONSE" == "yes" || "$LOWER_CASE_RESPONSE" == "y" ]]; then
     if [[ $(command -v brew) ]]; then
       brew install az
     else
-      echo "please Install az cli using https://learn.microsoft.com/en-us/cli/azure/install-azure-cli"
+      echo "Please Install az cli using https://learn.microsoft.com/en-us/cli/azure/install-azure-cli"
     fi
   fi
 else
